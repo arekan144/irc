070ebe276daef1c6fc7bc047f05259c256bf35f6
arekan144 <75382499+arekan144@users.noreply.github.com>
2021-06-07 10:40:09 +0200

    Update senddata.js

--- static/js/classes/senddata.js
+++ static/js/classes/senddata.js
@@ -1,14 +1,17 @@
 "use strict";
 
 export default class SendData {
-    constructor(nick, message, url) {
+    constructor(nick, message, url, de) {
         this.data = { nick: nick, message: message }
         this.url = "";
         if (url) {
             this.url = url;
         }
         this.address = "https://irc-arkadiusz-sala.herokuapp.com/"
+        if(de)
         this.sendIt();
+        else this.sendIt2();
+            
     }
     sendIt = () => {
         fetch(this.address + this.url, {
@@ -17,6 +20,17 @@ export default class SendData {
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(this.data)
+        });
+    }
+    sendIt2 = () => {
+        fetch(this.address + this.url, {
+            method: 'POST',
+            
+            headers: {
+                'Content-Type': 'application/json',
+                
             },
             body: JSON.stringify(this.data)
         });
