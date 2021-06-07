4277b45101843af1ae73de17ee12fd3b02a2a3e0
arekan144 <75382499+arekan144@users.noreply.github.com>
2021-06-07 10:23:48 +0200

    Update getdata.js

--- static/js/classes/getdata.js
+++ static/js/classes/getdata.js
@@ -15,7 +15,8 @@ export default class LongPoolAwait {
             method: 'POST',
             
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
             },
             body: "{}",
         });
