47db38ea12bba7e2fc7da19cfdc5d2ce44d524a6
arekan144 <75382499+arekan144@users.noreply.github.com>
2021-06-07 09:57:41 +0200

    Update senddata.js

--- static/js/classes/senddata.js
+++ static/js/classes/senddata.js
@@ -13,6 +13,7 @@ export default class SendData {
     sendIt = () => {
         fetch(this.address + this.url, {
             method: 'POST',
+            mode: 'no-cors',
             headers: {
                 'Content-Type': 'application/json'
             },
